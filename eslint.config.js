f608bbcd430f25ba3e63fcd34254111bc290b8aa
우형래 <174302871+hyung-rae@users.noreply.github.com>
2025-06-19 17:43:29 +0900

    ♻️ Update: storybook package + eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,30 @@
 import js from '@eslint/js'
 import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import react from 'eslint-plugin-react'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+import { defineConfig } from 'eslint/config'
+import typescriptParser from '@typescript-eslint/parser'
 
-export default [
-  { ignores: ['dist'] },
+export default defineConfig([
+  { files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'], plugins: { js }, extends: ['js/recommended'] },
+  { files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'], languageOptions: { globals: globals.browser } },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
   {
-    files: ['**/*.{js,jsx}'],
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parser: typescriptParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
       parserOptions: {
-        ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
-        sourceType: 'module',
       },
     },
-    plugins: {
-      'react': react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     rules: {
-      ...js.configs.recommended.rules,
-      ...reactHooks.configs.recommended.rules,
-      'react/jsx-uses-react': 'error',
-      'react/jsx-uses-vars': 'error',
-      'no-unused-vars': [
+      '@typescript-eslint/no-unused-vars': [
         'error',
         {
           vars: 'all',
@@ -37,7 +34,9 @@ export default [
           reportUsedIgnorePattern: false,
         },
       ],
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      // Disable the base rule as it can report incorrect errors
+      'no-unused-vars': 'off',
+      'react/react-in-jsx-scope': 'off',
     },
   },
-]
+])
